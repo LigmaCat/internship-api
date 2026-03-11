28c9fc11d0762e40c6444ed6feaf8a755768472b
Your Name <you@example.com>
2026-03-11 10:46:12 +0200

    added seeder for roles

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ApplicationController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('companies', CompanyController::class);
+Route::apiResource('applications', ApplicationController::class);
